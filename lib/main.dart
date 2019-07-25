e4f61295ee6e7b5078f8a972ba34a934af76bafb
“Rishabh <devrishabh@Rishabhs-MacBook-Pro.local>
2019-07-25 14:06:14 +0200

    mod

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -28,16 +29,24 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> quizScore = [];
 
-  List<String> quizQuestions = [
+  static List<String> quizQuestions = [
     'You can lead a cow down stairs but not up stairs.',
     'Approximately one quarter of human bones are in the feet.',
     'A slug\'s blood is green.',
   ];
 
-  List<bool> answers = [
+  static List<bool> answers = [
     false,
     true,
     true,
+  ];
+
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
 
   int questionNumber = 0;
